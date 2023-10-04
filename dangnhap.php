6bebe668bf853cd816afe2a0b445529e177f277a
kieuhann <105109200+kieuhann@users.noreply.github.com>
2023-10-04 11:25:57 +0700

    abc

--- dangnhap.php
+++ dangnhap.php
@@ -1,40 +1,32 @@
 <?php
 
 include("connect.php");
-                if(isset($_POST["sb1"])){
+                if(isset($_POST["taxi"])){
                     
-                    $sql="SELECT * FROM nguoidung where email='".$_POST["email"]."' AND matkhau='".md5($_POST["password"])."'";
+                    $sql="SELECT * FROM khachhang where user='".$_POST["user"]."' AND matkhau='".md5($_POST["password"])."'";
                     $result1 = $conn->query($sql);
                   // print_r($result1);
                     if($result1->num_rows>0){
                         
                         $row = $result1->fetch_assoc();
                         
-                        $_SESSION["email"] = $row["EMAIL"];
-                        $_SESSION["password"]=$row["MATKHAU"];
-                        $_SESSION["diachi"]=$row["DIACHI"];
-                        $_SESSION["hoten"]=$row["TEN"];
-                        $fullname = explode(' ', $row['TEN']);
-                        $lastname = end($fullname);
-                        $_SESSION["lname"] = $lastname;
+                        $_SESSION["ma"] = $row["MA"];
+                        $_SESSION["QH_ma"] = $row["QH_MA"];
+                        $_SESSION["ten"]=$row["TEN"];
                         $_SESSION["sdt"]=$row["SDT"];
-                        $_SESSION['slsp'] = 0;
+                        $_SESSION["email"]=$row["EMAIL"];
+                        $_SESSION["usesname"] = $row["USESNAME"];
+                        $_SESSION["password"]=$row["PASSWORD"];
+                        $_SESSION["gioitinh"] = $row["GIOITINH"];
                    
                         header('Location: index.php');
-                     
-/*
-                        echo $_SESSION["name"]."<br>==";
-                        echo $_SESSION["ngaysinh"]."<br>==";
-                        echo $_SESSION["sdt"];
-                        
-    */ 
                     }
                     else{
                  
                         echo '<script language="javascript">
                     alert("Nhập sai email hoặc mật khẩu.");
                     history.back();
-                     </script>';
+                    </script>';
                       
                     }
                    
