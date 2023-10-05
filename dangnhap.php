ad82ee8e727494d0e14bc2aac89814d4b8e01aa2
Trần Ngọc Duy <duybii922002@gmail.com>
2023-10-05 18:30:40 +0700

    update form đăng nhập, chỉnh sửa thông tin

--- dangnhap.php
+++ dangnhap.php
@@ -1,37 +1,50 @@
 <?php
 
 include("connect.php");
-                if(isset($_POST["dangnhap"])){
 
-                    echo $_POST["username"].' '.md5($_POST["psw"]);
-                    
-                    $sql="SELECT * FROM khachhang where KH_USERNAME='".$_POST["username"]."' AND KH_PASSWORD='".md5($_POST["psw"])."'";
-                    $result1 = $conn->query($sql);
-                  // print_r($result1);
-                    if($result1->num_rows>0){
-                        
-                        $row = $result1->fetch_assoc();
-                        
-                        $_SESSION["kh_ma"] = $row["KH_MA"];
-                        $_SESSION["qh"] = $row["QH_MA"];
-                        $_SESSION["ten"]=$row["KH_TEN"];
-                        $_SESSION["sdt"]=$row["KH_SDT"];
-                        $_SESSION["email"]=$row["KH_EMAIL"];
-                        $_SESSION["username"] = $row["KH_USERNAME"];
-                        $_SESSION["psw"]=$row["KH_PASSWORD"];
-                        $_SESSION["gioitinh"] = $row["KH_GIOITINH"];
-                   
-                        header('Location: index.php');
-                    }
-                    else{
-                 
-                    //     echo '<script language="javascript">
-                    // alert("Nhập sai email hoặc mật khẩu.");
-                    // history.back();
-                    // </script>';
-                      
-                    }
-                   
-                    }
-                
-                ?>
+if (isset($_POST["dangnhap"])) {
+    $username = $_POST["username"];
+    $password = $_POST["psw"];
+
+    $sql = "SELECT * FROM khachhang WHERE KH_USERNAME = ?";
+
+    // Sử dụng câu lệnh chuẩn bị để tránh SQL Injection
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $hashed_password = md5($password); // Mã hóa mật khẩu đầu vào
+
+        if ($hashed_password === $row["KH_PASSWORD"]) {
+          echo $hashed_password.' '.$row["KH_PASSWORD"];
+            // Đăng nhập thành công, thiết lập các biến session
+            $_SESSION["kh_ma"] = $row["KH_MA"];
+            $_SESSION["qh"] = $row["QH_MA"];
+            $_SESSION["ten"] = $row["KH_TEN"];
+            $_SESSION["sdt"] = $row["KH_SDT"];
+            $_SESSION["email"] = $row["KH_EMAIL"];
+            $_SESSION["username"] = $row["KH_USERNAME"];
+            $_SESSION["psw"] = $row["KH_PASSWORD"];
+            $_SESSION["gioitinh"] = $row["KH_GIOITINH"];
+
+            echo '<script language="javascript">
+            alert("Đăng nhập thành công!");
+            window.location.href = "index.php"; // Chuyển hướng sau khi đăng ký thành công
+            </script>';
+            
+            exit();
+        } else {
+          
+
+            echo "Nhập sai mật khẩu.";
+        }
+    } else {
+        echo "Tên đăng nhập không tồn tại.";
+    }
+
+    $stmt->close();
+}
+?>
