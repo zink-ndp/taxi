7c3454c701fe35e1f06b59a0ecec3ddf118dff3c
kieuhann <105109200+kieuhann@users.noreply.github.com>
2023-10-21 00:52:40 +0700

    suatt

--- suathongtin.php
+++ suathongtin.php
@@ -1,66 +1,103 @@
-<?php
-$activate = "contact";
-// @include ('connect.php');
-@include('header.php');
+<?php 
+include("header.php");
 ?>
-    
-    <section class="hero-wrap hero-wrap-2 js-fullheight" style="background-image: url('images/bg_3.jpg');" data-stellar-background-ratio="0.5">
-      <div class="overlay"></div>
-      <div class="container">
-        <div class="row no-gutters slider-text js-fullheight align-items-end justify-content-start">
-          <div class="col-md-9 ftco-animate pb-5">
-          	<p class="breadcrumbs"><span class="mr-2"><a href="index.html">Trang chủ<i class="ion-ios-arrow-forward"></i></a></span> <span>Thông tin cá nhân<i class="ion-ios-arrow-forward"></i></span></p>
-            <h1 class="mb-3 bread">Thông tin cá nhân</h1>
-          </div>
+<head>
+    <title>Thay đổi thông tin người dùng</title>
+    <!-- <link rel="stylesheet" href="css/style.css"> -->
+
+</head>
+<style>
+.ftco-navbar-light {
+    background: brown;
+    position: absolute;
+    top: 20px;
+    left: 0;
+    right: 0;
+    z-index: 3;
+}
+.col-6 {
+    -webkit-box-flex: initial;
+    -ms-flex: 0 0 50%;
+    flex: revert;
+    max-width: 100%;
+}
+  input, button, select, optgroup, textarea {
+    margin:inherit;
+    font-family: inherit;
+    background-color:lightblue;
+}
+
+.container-fluid {
+    width: max-content;
+    padding-right: 20px;
+    padding-left: 20px;
+    margin-right: revert;
+    margin-left: inherit;
+}
+
+body {
+    font-family: "Poppins", Arial, sans-serif;
+    background: #cccccc36;
+    font-size: 18px;
+    line-height: normal;
+    font-weight: 2000;
+    text-align: -webkit-right;
+    color:darkgray;
+}
+h1, h2, h3, h4, h5, .h1, .h2, .h3, .h4, .h5 {
+    line-height: 1;
+    color: rgba(0, 0, 0, 1);
+    font-weight: 300;
+}
+h1, .h1 {
+    font-size: 3;
+}
+h1, h2, h3, h4, h5, h6, .h1, .h2, .h3, .h4, .h5, .h6 {
+    margin-bottom: 2rem;
+    font-weight: 500;
+    line-height: 1.2;
+}
+h1, h2, h3, h4, h5, h6 {
+    margin-top: 5px;
+    margin-bottom: 0.5rem;
+}
+</style>
+<body>
+<section class="ftco-section contact-section">
+    <div class="container">
+        <div class="container-fluid pt-1">
+            <div class="row px-xl-5">
+                <div class="col-lg-12 mb-5" style="margin-top: -40px;">
+                    <section class="text-again:center">
+                        <div class="row">
+                            <div class="col-6">
+                            <h1>Thay đổi thông tin người dùng</h1>
+                              <form action="sua.php" method="post">
+                                  <label for="username">Username:</label>
+                                  <input type="text" name="username" required><br>
+
+                                  <label for="full_name">Họ tên:</label>
+                                  <input type="text" name="full_name"><br>
+
+                                  <label for="phone">Số điện thoại:</label>
+                                  <input type="text" name="phone"><br>
+
+                                  <label for="email">Email:</label>
+                                  <input type="email" name="email"><br>
+
+                                  <input type="submit" value="Cập nhật">
+                              </form>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
         </div>
-      </div>
-    </section>
-
-    <section class="ftco-section contact-section">
-      <div class="container">
-        <div class="row d-flex mb-5 contact-info">
-
-          <div class="col-md-8 block-9 mb-md-5">
-          <form class="row g-3 formdangky" action="suathongtin.php" method="POST">
-          <h1 class="text-again: center;">Thông tin cá nhân</h1>
-
-          <div class="col-md-12" >
-          <label for="inputAddress" class=" font-weight: bold"></label>Tên đăng nhập:<?php echo $_SESSION["username"]?>
-          </label></div>
-
-          <div class="col-md-12">
-              <label for="inputAddress" class="form-label">Email<span class="error">*</span> </label>
-              <input type="email" class="form-control" id="email"  value="<?php echo $_SESSION["email"]?>" placeholder="Nhập địa chỉ email của bạn" name="email">
-          </div>
-          <div class="col-md-6">
-              <label for="inputAddress" class="form-label">Họ và tên<span class="error">*</span> </label>
-              <input type="text" class="form-control"  value="<?php echo  $_SESSION["ten"] ?>" id="inputAddress" placeholder="Tên" name="ten">
-          </div>
-
-          <div class="col-md-6">
-            <label for="inputNumberl4" class="form-label">Số điện thoại<span class="error">*</span></label>
-            <input type="text" class="form-control" id="sdt" value="<?php echo $_SESSION["sdt"]?>" placeholder="Nhập địa chỉ số điện thoại của bạn" name="sdt">
-          </div>
-
-        </div>
-        <div class="col-md-12">      
-            <button type="submit" class="mt-2 btn btn-success"  name="luuthongtin">Lưu thông tin </button>
-          </div>
-      </div>
-
     </div>
 </section>
-	
+   
+</body>
 
-<div class="row d-flex mb-5 contact-info">
-  <div class="col-md-8 block-9 mb-md-5">
-    <h2>&emsp;</h2>
-      <form action="suathongtin.php" name="frm-login" method="post">
-      </form>
-
-
-
-
-<?php
-@include('footer.php');
+<?php 
+include("footer.php");
 ?>
