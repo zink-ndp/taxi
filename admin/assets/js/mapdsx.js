d4b2c021797fb43bce26dba2cb93353049bd2938
Trần Ngọc Duy <duybii922002@gmail.com>
2023-11-07 14:02:49 +0700

    update hiện đường đi

--- admin/assets/js/mapdsx.js
+++ admin/assets/js/mapdsx.js
@@ -1,14 +1,24 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'taxi'
+});
+
+connection.connect();
+
+const sqlQuery = 'SELECT CX_TDDIEMDI_X, CX_TDDIEMDI_Y, CX_TDDIEMDEN_X, CX_TDDIEMDEN_Y FROM chuyenxe';
+
 // Đường dẫn tới hình ảnh của xe ô tô
 const carIconUrl = 'assets/img/iconxe.png';
 
-// Tạo biểu tượng xe ô tô bằng hình ảnh
 const carIcon = L.icon({
     iconUrl: carIconUrl,
-    iconSize: [40, 50], // Kích thước của biểu tượng
-    iconAnchor: [15, 15] // Điểm neo
+    iconSize: [40, 50],
+    iconAnchor: [15, 15]
 });
 
-// mapdsx.js
 const map = L.map('mapds').setView([10.03, 105.77], 15);
 
 const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -28,42 +38,51 @@ map.on('click', onMapClick);
 // Truy cập dữ liệu JSON từ biến jsonData
 console.log(jsonData);
 
-// Hàm ánh xạ giá trị trạng thái
 function mapTrangThai(trangThai) {
     return trangThai === '1' ? 'Rảnh' : 'Bận';
 }
 
-// ...
-// Ví dụ: Làm việc với dữ liệu JSON
+// Lấy tọa độ đích từ cơ sở dữ liệu và sử dụng nó
 jsonData.forEach(function (item) {
     const marker = L.marker([item.TT_TOADOX, item.TT_TOADOY], {
-        icon: carIcon // Sử dụng biểu tượng xe ô tô
+        icon: carIcon
     }).addTo(map);
 
-    // Ánh xạ giá trị trạng thái
     const trangThai = mapTrangThai(item.TT_TRANGTHAI);
 
-    // Tạo popup cho mỗi marker
     const popup = L.popup()
         .setLatLng([item.TT_TOADOX, item.TT_TOADOY])
-        .setContent(`<b>Tên tài xế:</b> ${item.TX_TEN}<br><b>Trạng thái:</b> ${trangThai}<br><b>Tọa độ:</b> ${item.TT_TOADOX},<b></b> ${item.TT_TOADOY}`);
+        .setContent(`<b>Tên tài xế:</b> ${item.TX_TEN}<br><b>Trạng thái:</b> ${trangThai}<br><b>Tọa độ:</b> ${item.TT_TOADOX}, ${item.TT_TOADOY}`);
     
     marker.on('click', function() {
         popup.openOn(map);
-        route = L.Routing.control({
-        waypoints: [
-            L.latLng(item.TT_TOADOX, item.TT_TOADOY),
-            L.latLng(10.02914,105.77167)
-            
-        ],
-        draggableWaypoints: false,
-        routeWhileDragging: false,
-        fitSelectedRoutes: false,
-        lineOptions: {
-            styles: [{ color: '#19d600', opacity: 0.6, weight: 6 }]
-        },
-        createMarker: function () {return null}
-    }).addTo(map)
+
+        // Truy vấn SQL để lấy tọa độ đích từ cơ sở dữ liệu
+        const sqlDestinationQuery = `SELECT CX_TDDIEMDEN_X, CX_TDDIEMDEN_Y FROM chuyenxe WHERE TX_MA = ${item.TX_MA} AND CX_TRANGTHAI ="1" ORDER BY TD_DATE DESC `;
+        connection.query(sqlDestinationQuery, function (error, results) {
+            if (error) throw error;
+
+            if (results.length > 0) {
+                const destinationLatitude = results[0].CX_TDDIEMDEN_X;
+                const destinationLongitude = results[0].CX_TDDIEMDEN_Y;
+
+                // Tạo đường dẫn sử dụng tọa độ đích từ cơ sở dữ liệu
+                route = L.Routing.control({
+                    waypoints: [
+                        L.latLng(destinationLatitude, destinationLongitude),
+                        L.latLng(item.TT_TOADOX, item.TT_TOADOY)
+                    ],
+                    draggableWaypoints: false,
+                    routeWhileDragging: false,
+                    fitSelectedRoutes: false,
+                    lineOptions: {
+                        styles: [{ color: '#19d600', opacity: 0.6, weight: 6 }]
+                    },
+                    createMarker: function () { return null; }
+                }).addTo(map);
+            }
+        });
     });
-    
 });
+
+connection.end();
